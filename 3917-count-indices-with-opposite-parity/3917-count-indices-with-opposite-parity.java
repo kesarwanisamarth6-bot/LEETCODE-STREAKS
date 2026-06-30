a375c3c7cb2f93380a8a11992516d6d5bea0a8ac
Samarth kesarwani <kesarwanisamarth6@gmail.com>
2026-06-30 23:45:28 +0530

    Time: 1 ms (100%), Space: 46.9 MB (48.02%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
@@ -1,31 +1,54 @@
 class Solution {
     public int[] countOppositeParity(int[] nums) {
-        String ans;
-        int count;
+        // String ans;
+        // int count;
+        // for(int i =0;i<nums.length;i++){
+        //  if(nums[i]%2==0){
+        //      ans = "even";
+        //  }
+        //     else{
+        //         ans = "odd";
+        //     }
+        //     count = 0;
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(ans.equals("odd")){
+        //             int n = nums[j];
+        //             if(n%2==0){
+        //                 count++;
+        //             }
+        //         }
+        //         else {
+        //             int n = nums[j];
+        //             if(n%2!=0){
+        //                 count++;
+        //             }
+        //         } 
+        //     }
+        //     nums[i]=count;
+        // }
+        // return nums;
+
+        int odd = 0;
+        int even = 0;
         for(int i =0;i<nums.length;i++){
-         if(nums[i]%2==0){
-             ans = "even";
-         }
+            if(nums[i]%2==0){
+                even++;
+            }
             else{
-                ans = "odd";
+                odd++;
             }
-            count = 0;
-            for(int j=i+1;j<nums.length;j++){
-                if(ans.equals("odd")){
-                    int n = nums[j];
-                    if(n%2==0){
-                        count++;
-                    }
-                }
-                else {
-                    int n = nums[j];
-                    if(n%2!=0){
-                        count++;
-                    }
-                } 
-            }
-            nums[i]=count;
         }
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]%2!=0){
+                    nums[i]=even;
+                    odd--;
+                }
+                else{
+                    nums[i]=odd;
+                    even--;
+                }
+            }
+        
         return nums;
     }
 }
