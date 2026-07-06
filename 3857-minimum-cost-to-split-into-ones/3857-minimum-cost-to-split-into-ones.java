2e219dcd316f3713786ca84d78a2f3f092d3441e
Samarth kesarwani <kesarwanisamarth6@gmail.com>
2026-07-07 03:05:21 +0530

    Time: 1 ms (33.29%), Space: 42.4 MB (45.97%) - LeetHub

---
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int solve(int n) {
+        if (n == 1) {
+            return 0;
+        }
+
+        int a, b;
+
+        if (n % 2 == 0) {
+            a = n / 2;
+            b = n / 2;
+        } else {
+            a = 1;
+            b = n - 1;
+        }
+
+        return (a * b) + solve(a) + solve(b);
+    }
+
+    public int minCost(int n) {
+        return solve(n);
+    }
+}
