e7540c8935ee47ca2d5c2c98fda2e12d89010c38
Samarth kesarwani <kesarwanisamarth6@gmail.com>
2026-08-09 11:36:35 +0530

    Time: 12 ms (5.49%), Space: 45.6 MB (10.4%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void solve(int arr[],int i, List<List<Integer>>ans,List<Integer>path){
+        if(i==arr.length){
+            if(!ans.contains(path)){
+                ans.add(new ArrayList(path));
+            }
+            return;
+        }
+
+        List<Integer> path1 = new ArrayList(path);
+        List<Integer> path2 = new ArrayList(path);
+        path2.add(arr[i]);
+        solve(arr,i+1,ans,path1);
+        solve(arr,i+1,ans,path2);
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
+        Arrays.sort(nums);
+
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    int i =0;
+    solve(nums,i,ans,path);
+    return ans;
+    }
+}
