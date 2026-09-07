9c0aa9c13ac71b44d4c504abb1ce11acdcd2a1fe
Samarth kesarwani <kesarwanisamarth6@gmail.com>
2026-09-07 22:29:52 +0530

    Time: 7 ms (88.94%), Space: 46 MB (78.82%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,13 +1,17 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-    HashMap<Character,Integer> map = new HashMap<>();
+    // HashMap<Character,Integer> map = new HashMap<>();
+    int freq[] =  new int[26];
     int i = 0;
     int j = 0;
     int max = 0;
+    int maxFreq= 0 ;
     while(j<s.length()){
         char c = s.charAt(j);
-        map.put(c,map.getOrDefault(c,0)+1);
-        int maxFreq = Collections.max(map.values());
+            freq[c-'A']++;
+            maxFreq = Math.max(freq[c-'A'],maxFreq);
+        // map.put(c,map.getOrDefault(c,0)+1);
+        // int maxFreq = Collections.max(map.values());
         int replace = (j-i+1) - maxFreq;
         if(replace<=k){
             max = Math.max(j-i+1,max);
@@ -16,13 +20,18 @@ class Solution {
         else {
             while(replace>k){
                 char rem = s.charAt(i);
-                map.put(rem,map.get(rem)-1);
-                if(map.get(rem)==0){
-                    map.remove(rem);
-                }
-                i++;
-               maxFreq = Collections.max(map.values());
-               replace = (j-i+1) - maxFreq;
+            //     map.put(rem,map.get(rem)-1);
+            //     if(map.get(rem)==0){
+            //         map.remove(rem);
+            //     }
+            //     i++;
+            //    maxFreq = Collections.max(map.values());
+            //    replace = (j-i+1) - maxFreq;
+            // }
+            freq[rem-'A']--;
+            i++;
+            maxFreq = Math.max(freq[c-'A'],maxFreq);
+             replace = (j-i+1) - maxFreq;
             }
             j++;
         }
