8ae9da3a72366caa9f61e0e9b292c7da48d45eb6
Samarth kesarwani <kesarwanisamarth6@gmail.com>
2026-09-14 23:44:58 +0530

    Time: 0 ms (100%), Space: 42.7 MB (52.92%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int x1 = Math.max(rec1[0],rec2[0]);  
+          int x2 = Math.min(rec1[2],rec2[2]);
+
+            int y1 = Math.max(rec1[1],rec2[1]);
+            int y2 = Math.min(rec1[3],rec2[3]);
+
+            int widht = x2-x1;
+            int height = y2-y1;
+            return widht> 0 && height> 0;
+        
+
+    }
+}
