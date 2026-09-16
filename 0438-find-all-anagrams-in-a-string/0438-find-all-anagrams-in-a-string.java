c2ed6523bfd67a475e0d3961963e40c5c80a6f42
Samarth kesarwani <kesarwanisamarth6@gmail.com>
2026-09-16 16:59:52 +0530

    Time: 12 ms (51.99%), Space: 47 MB (45.94%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,32 +1,88 @@
 class Solution { 
     public List<Integer> findAnagrams(String s, String p) {
-    List<Integer> list = new ArrayList<>();
-    String str = p;
-    char [] num = str.toCharArray();
-    Arrays.sort(num);
-    String newp = new String(num);
-    int k = p.length();
-    StringBuilder  sb = new StringBuilder();
-    int i = 0;
-    int j = 0;
-    while(j<s.length()){
-        sb.append(s.charAt(j));
+        List<Integer> list = new ArrayList<>();
+        int pcount[] = new int[26];
+        int scount[] = new int[26];
+        int k = p.length();
+        for(int i = 0;i<p.length();i++){
+            char c = p.charAt(i);
+            pcount[c-'a']++;
+        }
 
-        if(j-i+1<k){
-        j++;
+        int i = 0;
+        int j = 0;
+        while(j<s.length()){
+            char add = s.charAt(j);
+            scount[add-'a']++;
+
+            if(j-i+1<k){
+                j++;
+            }
+            else if (j-i+1==k){
+            if(Arrays.equals(scount,pcount)){
+                list.add(i);
+            }
+            char rem = s.charAt(i);
+            scount[rem-'a']--;
+            i++;
+            j++;
+            }
         }
-        else if (j-i+1==k){
-        char arr[] = sb.toString().toCharArray();
-        Arrays.sort(arr);
-        String sortedsb = new String(arr);
-        if(sortedsb.equals(newp)){
-            list.add(i);
-        }
-        sb.deleteCharAt(0);
-        i++;
-        j++;
-        }
-    }
-    return list;
+
+
+        return list;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // List<Integer> list = new ArrayList<>();
+    // String str = p;
+    // char [] num = str.toCharArray();
+    // Arrays.sort(num);
+    // String newp = new String(num);
+    // int k = p.length();
+    // StringBuilder  sb = new StringBuilder();
+    // int i = 0;
+    // int j = 0;
+    // while(j<s.length()){
+    //     sb.append(s.charAt(j));
+
+    //     if(j-i+1<k){
+    //     j++;
+    //     }
+    //     else if (j-i+1==k){
+    //     char arr[] = sb.toString().toCharArray();
+    //     Arrays.sort(arr);
+    //     String sortedsb = new String(arr);
+    //     if(sortedsb.equals(newp)){
+    //         list.add(i);
+    //     }
+    //     sb.deleteCharAt(0);
+    //     i++;
+    //     j++;
+    //     }
+    // }
+    // return list;
     }
 }
